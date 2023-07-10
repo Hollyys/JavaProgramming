57344cc05c97e3ced97eda0579af3b4cebc9619d
Sunghan Shin <crossrunway01@gmail.com>
2023-07-10 22:24:05 +0900

    Ch15/HashMapStudentEx

--- 2023_JavaProgramming/src/Chapter15/HashMapStudentEx.java
+++ 2023_JavaProgramming/src/Chapter15/HashMapStudentEx.java
@@ -18,9 +18,12 @@ public class HashMapStudentEx {
 		while(true) {
 			System.out.print("검색할 이름? ");
 			String name = scanner.nextLine(); // 사용자로부터 이름 입력
+			
 			if(name.equals("exit"))
 				break; // while 문을 벗어나 프로그램 종료
+			
 			Student student = map.get(name); //이름에 해당하는 Student 객체 검색
+			
 			if(student == null)
 				System.out.println(name + "은 없는 사람입니다.");
 			else
--- 2023_JavaProgramming/src/Chapter15/Student.java
+++ 2023_JavaProgramming/src/Chapter15/Student.java
@@ -3,13 +3,16 @@ package Chapter15;
 public class Student {
 	private int id;
 	private String tel;
+	
 	public Student(int id, String tel) {
 		this.id = id;
 		this.tel = tel;
 	}
+	
 	public int getId() {
 		return id;
 	}
+	
 	public String getTel() {
 		return tel;
 	}
