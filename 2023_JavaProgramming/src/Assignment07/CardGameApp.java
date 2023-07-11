bce3dc569e8d5ab804f73869464172d0d133cd8d
Sunghan Shin <crossrunway01@gmail.com>
2023-07-11 21:00:17 +0900

    Ass07/CardGameApp.java

--- 2023_JavaProgramming/src/Assignment07/CardGameApp.java
+++ 2023_JavaProgramming/src/Assignment07/CardGameApp.java
@@ -61,13 +61,7 @@ class CardDealer{
 	
 	public void deal(Player p1, Player p2, Player p3) {
 
-		for (int i = 0; i < 3; i++) {
-			p1.holdingCardList.add(initialCard.remove(0));
-			p2.holdingCardList.add(initialCard.remove(0));
-			p3.holdingCardList.add(initialCard.remove(0));
-		}
-		
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < 2; i++) {
 			p1.holdingCardList.add(initialCard.remove(0));
 			p2.holdingCardList.add(initialCard.remove(0));
 			p3.holdingCardList.add(initialCard.remove(0));
@@ -111,18 +105,33 @@ class Player{
 	ArrayList<Card> openCardList = new ArrayList<>();
 	
 	public void generateOpenCard() {
-		for (int i = 0; i < holdingCardList.size(); i++) {
+		
+		Card card = new Card("null", "null");
+		
+		for (int i = 0; i < holdingCardList.size()-1; i++) {
+			
 			Card card1 = holdingCardList.get(i);
-			for(int j = i+1; j < holdingCardList.size(); j++) {
+			
+			for(int j = i+1; j < holdingCardList.size();j++) {
 				Card card2 = holdingCardList.get(j);
-				if(card1.numString.equals(card2.numString)){
+				if(card1.numString.equals(card2.numString) && !card1.numString.equals("null")){
 					openCardList.add(card1);
 					openCardList.add(card2);
-					holdingCardList.remove(i);
-					holdingCardList.remove(j);
+					
+					holdingCardList.set(i, card);
+					holdingCardList.set(j, card);
 					break;
 				}
 			}
+		}
+		
+		int i = 0;
+		while(i < holdingCardList.size()) {
+			Card card1 = holdingCardList.get(i);
+			if(card1.numString.equals("null"))
+				holdingCardList.remove(i);
+			else
+				i++;
 		}
 	}
 	
@@ -152,6 +161,9 @@ public class CardGameApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		String input;
+		
 		Player p1 = new Player("봉다리");
 		Player p2 = new Player("새롱");
 		Player p3 = new Player("몽실");
@@ -168,7 +180,7 @@ public class CardGameApp {
 		
 		int cnt = 1;
 		
-		System.out.printf("<< %2d 단계 >>\n",cnt);
+		System.out.printf("<< %2d 단계 >>  숫자가 같은 카드 공개(2 장씩 허용)\n\n",cnt);
 		p1.generateOpenCard();
 		p2.generateOpenCard();
 		p3.generateOpenCard();
@@ -177,27 +189,40 @@ public class CardGameApp {
 		while(true) {
 			cnt++;
 			System.out.printf("<< %2d 단계 >> 숫자가 같은 카드공개 (2장씩 허용)\n",cnt);
-			System.out.println(" - 딜러로부터 2 장씩 새로운 카드를 받습니다.");
+			System.out.println(" - 딜러로부터 2 장씩 새로운 카드를 받습니다.\n");
 			
 			cardDealer.deal(p1, p2, p3);
 			p1.generateOpenCard();
 			p2.generateOpenCard();
 			p3.generateOpenCard();
 			cardDealer.print_status(p1, p2, p3);
+			
+			if(cardDealer.initialCard.size() < 6) {
+				 // 게임 종료조건 1
+				 break;
+			 }
+			
+			if(p1.holdingCardList.size() <= 0
+					|| p2.holdingCardList.size() <= 0
+						|| p3.holdingCardList.size() <= 0) {
+				// 게임 종료조건 2
+				break;
+			}
+			
+			System.out.println("다음 단계 게임 진행을 위해 Enter 키를 누르세요!\n");
+			input = scanner.nextLine();
+			
+			if(input.equals("q"))
+				break;
 		}
+		
+		System.out.printf("경기 종료 (Dealer가 가진 카드수: %2d)\n", cardDealer.initialCard.size());
+		System.out.printf("짝을 맞춘 개수 (%s: %3d), (%s: %3d), (%s: %3d)"
+											, p1.name, p1.openCardList.size(), p2.name
+											, p1.openCardList.size()
+											, p3.name, p3.openCardList.size());
+		scanner.close();
 
 	}
 
 }
-
-//			 if(initialCard.size() < 6) {
-//				 // 게임 종료조건 1
-//				 break;
-//			 }
-//			 
-//			 if(p1.holdingCardList.size() == 0 ||
-//					 p2.holdingCardList.size() == 0 ||
-//					 p3.holdingCardList.size() == 0) {
-//				 // 게임 종료조건 2
-//				 break;
-//			 }
